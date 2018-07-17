354204442a362d42143bc8729262a136d278136b
brilee8820 <brilee8820@gmail.com>
2018-07-17 19:17:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+class App < Sinatra::Base
+
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do 
+    "Brianna"
+  end
+  
+  get '/hometown' do 
+    "Northville"
+  end
+  
+  get '/favorite_song'
+    "Dope"
+  end
 end
+end
+
